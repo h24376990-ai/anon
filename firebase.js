dda6b264823446e1ae957e760fce972dc66216e9
h24376990-ai <h24376990@gmail.com>
2026-04-03 18:14:12 +0900

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,36 @@
+// Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+import { 
+  getAuth, 
+  signInAnonymously,
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+// Firebase設定
 const firebaseConfig = {
-
-apiKey: "ここ",
-authDomain: "ここ",
-projectId: "ここ",
-storageBucket: "ここ",
-messagingSenderId: "ここ",
-appId: "ここ"
-
+  apiKey: "AIzaSyA0R2KYt2MgJHaiYQ9oM8IMXhX9oj-Ky_c",
+  authDomain: "anon-chat-de585.firebaseapp.com",
+  projectId: "anon-chat-de585",
+  storageBucket: "anon-chat-de585.firebasestorage.app",
+  messagingSenderId: "1035093625910",
+  appId: "1:1035093625910:web:65ba2370a79f73e23b9c97",
+  measurementId: "G-0KWK7SRJHZ"
 };
 
+// 初期化
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+// Firestore
+const db = getFirestore(app);
+
+// Auth
+const auth = getAuth(app);
+
+// 匿名ログイン
+signInAnonymously(auth);
+
+// export
+export { db, auth, onAuthStateChanged };
