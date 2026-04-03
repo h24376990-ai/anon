7893d7969711e4a180b817e7f2b71820d60bded1
h24376990-ai <h24376990@gmail.com>
2026-04-03 17:08:54 +0900

    Update profile.js

--- profile.js
+++ profile.js
@@ -1,47 +1,28 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore, doc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { auth, db } from "./firebase.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA0R2KYt2MgJHaiYQ9oM8IMXhX9oj-Ky_c",
-  authDomain: "anon-chat-de585.firebaseapp.com",
-  projectId: "anon-chat-de585",
-  storageBucket: "anon-chat-de585.firebasestorage.app",
-  messagingSenderId: "1035093625910",
-  appId: "1:1035093625910:web:65ba2370a79f73e23b9c97"
-};
+import {
+doc,
+setDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+window.save = async function(){
 
-onAuthStateChanged(auth, async user => {
-  if (!user) {
-    location.href = "index.html";
-    return;
-  }
+const user = auth.currentUser;
 
-  const uid = user.uid;
-  const snap = await getDoc(doc(db, "users", uid));
-  if (!snap.exists()) return;
+const name = document.getElementById("name").value;
+const age = document.getElementById("age").value;
+const sex = document.getElementById("sex").value;
+const bio = document.getElementById("bio").value;
 
-  const data = snap.data();
-  document.getElementById("name").value = data.name || "";
-  document.getElementById("age").value = data.age || "";
-  document.getElementById("location").value = data.location || "";
-  document.getElementById("bio").value = data.bio || "";
+await setDoc(doc(db,"users",user.uid),{
+
+name,
+age,
+sex,
+bio
+
 });
 
-document.getElementById("saveBtn").onclick = async () => {
-  const user = auth.currentUser;
-  if (!user) return;
+alert("保存しました");
 
-  await updateDoc(doc(db, "users", user.uid), {
-    name: document.getElementById("name").value,
-    age: document.getElementById("age").value,
-    location: document.getElementById("location").value,
-    bio: document.getElementById("bio").value
-  });
-
-  alert("保存しました");
-};
+}
